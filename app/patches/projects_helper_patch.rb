e87e6267e92062dc0c939ed0ff371908aaf86116
isma-data <dominik.pegler@inject-star.com>
2020-11-26 16:17:30 +0100

    alias_method_chain removal

--- app/patches/projects_helper_patch.rb
+++ app/patches/projects_helper_patch.rb
@@ -5,24 +5,26 @@ module SendIssueReplyEmail
     unloadable
 
     extend ActiveSupport::Concern
-
-    included do
-      alias_method_chain :project_settings_tabs, :email_delivery_setting_of_issue_reply_tab
-    end
-
-    def project_settings_tabs_with_email_delivery_setting_of_issue_reply_tab
-      tabs = project_settings_tabs_without_email_delivery_setting_of_issue_reply_tab
-
-      if User.current.allowed_to?(:manage_email_delivery_setting, @project) &&
+   
+    module Overrides
+      def project_settings_tabs(*args)
+        tabs = project_settings_tabs_without_email_delivery_setting_of_issue_reply_tab
+        if User.current.allowed_to?(:manage_email_delivery_setting, @project) &&
           @project.module_enabled?(:send_issue_reply_email)
 
-        tabs << {
-          name: 'email_delivery_setting_of_issue_reply',
-          action: :manage_email_delivery_setting,
-          partial: 'projects/settings/email_delivery_setting_of_issue_reply',
-          label: :label_send_issue_reply_email
-        }
+          tabs << {
+            name: 'email_delivery_setting_of_issue_reply',
+            action: :manage_email_delivery_setting,
+            partial: 'projects/settings/email_delivery_setting_of_issue_reply',
+            label: :label_send_issue_reply_email
+          }
+        super
       end
+    end
+
+    included do
+      prepend Overrides
+    end
 
       tabs
     end
